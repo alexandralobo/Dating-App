75911ca675576fbd8d367a6f28ff6be581ae95a0
Alexandra Lobo <A.Mota-Veiga-Magalhaes-Lobo@student.liverpool.ac.uk>
2021-08-27 19:30:52 +0100

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; } // it will recognised as PK [Id or ID]
+
+        public string UserName { get; set; }
+        
+    }
+}
