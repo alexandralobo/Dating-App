715c245c65af3fc6c18a17906330a9e362afa9a0
Alexandra Lobo <A.Mota-Veiga-Magalhaes-Lobo@student.liverpool.ac.uk>
2021-08-31 22:09:41 +0100

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,10 @@ namespace API.Entities
         public int Id { get; set; } // it will recognised as PK [Id or ID]
 
         public string UserName { get; set; }
-        
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
     }
 }
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,25 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // service will last until the end of HTTP request [AddScoped]
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
